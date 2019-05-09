da6e42663e51a88923b92420d6b5e617c92002a9
lvdinh <lvdinh@tma.com.vn>
2019-05-09 11:27:55 +0700

    Apply SSL

--- routes/index.js
+++ routes/index.js
@@ -7,24 +7,24 @@ router.get('/', function(req, res, next) {
   res.sendFile('/index.html', {root: path.join(__dirname, '..')});
   // res.redirect('https://sub1.' + req.host + ':' + normalizePort(process.env.PORT || '2000') + '/index');
 });
-router.get('/index', function(req, res, next) {
-  // res.sendFile('/index.html', {root: path.join(__dirname, '..')});
-});
+// router.get('/index', function(req, res, next) {
+//   res.sendFile('/index.html', {root: path.join(__dirname, '..')});
+// });
 
-function normalizePort(val) {
-  var port = parseInt(val, 10);
+// function normalizePort(val) {
+//   var port = parseInt(val, 10);
 
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
+//   if (isNaN(port)) {
+//     // named pipe
+//     return val;
+//   }
 
-  if (port >= 0) {
-    // port number
-    return port;
-  }
+//   if (port >= 0) {
+//     // port number
+//     return port;
+//   }
 
-  return false;
-}
+//   return false;
+// }
 
 module.exports = router;
